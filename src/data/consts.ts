dd4c7fd75c7d70f75f423d76d3c55f90bd3c49c5
lmj0415 <lassejantsch@web.de>
2025-10-09 15:00:47 +0900

    responsive design

---
+++ src/data/consts.ts
@@ -0,0 +1,25 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+import cv_data from './cv.json'
+
+// blog
+export const ACTIVATE_BLOG = true // activates the blog section of the website.
+
+// metadata
+export const SITE_TITLE = "LMJ's Profile";
+export const SITE_DESCRIPTION = "Lasse Jantsch's my portfolio website!";
+
+// Profile
+export const GREETING = "👋 Hey, I'm Lasse"
+export const INTRODUCTION = "A computer scientist with a background in economics specializing in the interpretability and reliability of Large Language Models. My research focuses on improving language models as reliable, large-scale text processing tools by leveraging the models internals for information extraction and task adaptation. Through my background in economics I ground my research not only in technical facination, but also in the social and economical implications of more reliable AI technology."
+export const CONTACT_AND_CV = "Please contact me at lassejantsch [at] knu.ac.kr."
+
+
+// Socials
+export const GITHUB = "https://github.com/LasseJantsch"
+export const LINKEDIN = "https://www.linkedin.com/in/lasse-jantsch-6985581a7"
+export const GOOGLESCHOLAR = "https://scholar.google.com/citations?user=FjrsTUUAAAAJ"
+
+// Work Experience
+export const CVITEMS = cv_data
