f62138e894704c5f3779e54a6d1eff98d872447f
lmj0415 <lassejantsch@web.de>
2025-10-17 10:17:12 +0900

    change redirect to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 import react from '@astrojs/react';
+import {ACTIVATE_BLOG} from './src/data/consts'
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,8 +20,12 @@ export default defineConfig({
 		plugins: [tailwindcss()],
 	},
 	image: {
-    service: {
-      entrypoint: 'astro/assets/services/noop',
-    },
-  },
+		service: {
+		entrypoint: 'astro/assets/services/noop',
+		}
+  	},
+	// redirect if blog is not activated
+	redirects: !ACTIVATE_BLOG && { 
+    	"/": "/profile",
+  	},
 });
